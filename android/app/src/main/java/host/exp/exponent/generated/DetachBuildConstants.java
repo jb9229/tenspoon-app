16cd8b249635c988385172d61ab226b2fe1c6743
jb9229 <jb9229@gmail.com>
2018-05-16 14:27:31 +0900

    initial commit   1) create-native-app   2) expo detach

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp26398b8c339b4673874738911ab9b5a8://tx-qtk.jb9229.tenspoon-app.exp.direct:80";
+
+}
